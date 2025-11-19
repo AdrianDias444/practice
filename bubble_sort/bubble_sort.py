d703aa9853b1703da131c5aa23e7c483367cf782
Adrian Dias <addias@c1r11s2.42lisboa.com>
2025-11-19 10:42:46 +0000

    bubble sort done

---
+++ bubble_sort/bubble_sort.py
@@ -0,0 +1,20 @@
+def bubble_sort(array):
+    n = len(array)
+    trocou = True
+
+    while trocou:
+        trocou = False
+        i = 0
+        while(i < n - 1):
+            if(array[i] > array[i + 1]):
+                temp = array[i + 1]
+                array[i + 1] = array[i]
+                array[i] = temp
+                trocou = True
+            i = i + 1
+
+
+array = [0, 3, 90, 1, 7, 2, 4, 11]
+bubble_sort(array)
+for i in range(len(array)):
+    print(array[i])
