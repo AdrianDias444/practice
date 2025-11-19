195bc7ca9273c28e935a40cb0ba82c3a8a509120
AdrianDias444 <adriandias0709@gmail.com>
2025-11-19 19:41:38 +0000

    update

--- insertion_sort/insertion_sort.py
+++ insertion_sort/insertion_sort.py
@@ -1,4 +1,4 @@
-'''
+"""
 Insertion Sort, divide o array tem 2 grupos
 - Sorted que comeca com apenas o 1 elemento
 - Unsorted que comeca com os restantes
@@ -6,26 +6,20 @@ Insertion Sort, divide o array tem 2 grupos
 - comparamos com todos os elementos do grupo Sorted
 (da direita para a esquerda), até encontrar a posição correta
 - onde ele deve ser inserido.
-'''
+"""
 
 
 def insertion_sort(array):
     n = len(array)
-    i  = 0
-    for i in range(n):
+    for i in range(n - 1):
         j = i + 1
-        i_temp, j_temp = i, j
-        while (array[j_temp] < array[i_temp] and j_temp > 0):
-            array[j_temp], array[i_temp] = array[i_temp], array[j_temp]
-            j_temp = j_temp - 1
-            i_temp = i_temp - 1
-        
-
-
+        while array[j] < array[j - 1] and j > 0:
+            array[j], array[j - 1] = array[j - 1], array[j]
+            j = j - 1
 
 
 array = [0, 3, 90, 1, 7, 2, 4, 11]
 insertion_sort(array)
-for i in range(len(range)):
+for i in range(len(array)):
     print(array[i])
 print("array len ", len(array))
