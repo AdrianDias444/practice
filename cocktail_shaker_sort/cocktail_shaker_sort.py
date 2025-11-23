b9c99d9b56ab3f6b1e151d406f8382867e05f173
AdrianDias444 <adriandias0709@gmail.com>
2025-11-23 19:37:14 +0000

    cocktail sort

---
+++ cocktail_shaker_sort/cocktail_shaker_sort.py
@@ -0,0 +1,32 @@
+def cocktail_shaker_sort(array):
+
+
+array = [
+    33,
+    201,
+    89,
+    242,
+    142,
+    199,
+    12,
+    54,
+    161,
+    177,
+    23,
+    212,
+    112,
+    155,
+    188,
+    74,
+    2,
+    44,
+    222,
+    64,
+    92,
+    239,
+    108,
+]
+
+cocktail_shaker_sort(array)
+for i in range(len(array)):
+    print(array[i])
