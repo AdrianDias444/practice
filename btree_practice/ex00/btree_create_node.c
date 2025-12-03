4db9cf7f04210656246dcb62ca9488c630704bd3
AdrianDias444 <adriandias0709@gmail.com>
2025-12-03 14:32:43 +0000

    btree practice ex00 done

--- btree_practice/ex00/btree_create_node.c
+++ btree_practice/ex00/btree_create_node.c
@@ -1,4 +1,31 @@
+#include "ft_btree.h"
+
 t_btree *btree_create_node(void *item)
 {
 
+    t_btree *new_node;
+
+    if(!item)       
+        return(NULL);
+
+    new_node = malloc(sizeof(t_btree));
+    if(!new_node)
+        return (NULL);
+    
+    new_node -> item = item;
+    new_node -> left = 0;
+    new_node -> right = 0;
+
+    return(new_node);
+}
+
+
+int main(void)
+{
+
+    char *str = "creating node";
+    char *node_content = btree_create_node(str)->item;
+    if(!node_content)
+        return(0);
+    printf("print content : %s", node_content);
 }
